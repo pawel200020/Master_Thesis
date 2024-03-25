a484ffc50ea05f0998a02d6e8a54466d2270eef3
pawel200020 <pawel200020@gmail.com>
2024-03-25 22:45:02 +0100

    final sql server query

--- DatabasesFiles/SQLServerDatabaseCreationQuery.sql
+++ DatabasesFiles/SQLServerDatabaseCreationQuery.sql
@@ -1,47 +1,84 @@
-CREATE TABLE Clients(
-	ClientId INT IDENTITY(1, 1) NOT NULL,
-	FirstName NVarChar (20) NOT NULL,
-	LastName NVarChar(20) NOT NULL,
-	PhoneNumber NVarChar(15),
-	Address XML NOT NULL,
-	Country NVarChar(20)
-)
+IF NOT EXISTS (SELECT name FROM master.sys.databases WHERE name = N'FrameworkPerformanceMT')
+Begin
+ CREATE DATABASE FrameworkPerformanceMT
+	COLLATE Latin1_General_100_CI_AI_SC_UTF8
+End
+Go
 
-CREATE TABLE Products(
-	ProductId INT IDENTITY(1, 1) NOT NULL,
-	ProductName NVarChar(50) NOT NULL,
-	ProductDescription NVarChar(MAX),
-	Price Decimal (18,2) NOT NULL,
-	Supplier NVarChar(50) NOT NULL
-)
+USE FrameworkPerformanceMT
+Go
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Clients') 
+BEGIN
+	CREATE TABLE Clients(
+		ClientId INT IDENTITY(1, 1) NOT NULL,
+		FirstName NVarChar (20) NOT NULL,
+		LastName NVarChar(20) NOT NULL,
+		PhoneNumber NVarChar(15),
+		Address XML NOT NULL,
+		Country NVarChar(20),
+		PRIMARY KEY (ClientId)
+	)
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Products')
+BEGIN
+	CREATE TABLE Products(
+		ProductId INT IDENTITY(1, 1) NOT NULL,
+		ProductName NVarChar(50) NOT NULL,
+		ProductDescription NVarChar(MAX),
+		Price Decimal (18,2) NOT NULL,
+		Supplier NVarChar(50) NOT NULL,
+		PRIMARY KEY (ProductId)
+	)
+END
+GO
 
-CREATE TABLE Stores(
-	StoreId INT IDENTITY(1, 1) NOT NULL,
-	Address NVarChar(100) NOT NULL,
-	Country NVarChar(100) NOT NULL
-)
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Stores')
+BEGIN
+	CREATE TABLE Stores(
+		StoreId INT IDENTITY(1, 1) NOT NULL,
+		Address NVarChar(100) NOT NULL,
+		Country NVarChar(100) NOT NULL,
+		PRIMARY KEY (StoreId)
+	)
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Positions')
+BEGIN
+	CREATE TABLE Positions (
+		PositionId INT IDENTITY(1, 1) NOT NULL,
+		PositionName NVarChar (100) NOT NULL,
+		CarParkingPlace NVarChar (20),
+		PositionBonus Decimal (18,2),
+		PRIMARY KEY (PositionId)
+	)
+END
+GO
 
-CREATE TABLE Positions (
-	PositionId INT IDENTITY(1, 1) NOT NULL,
-	PositionName NVarChar (100) NOT NULL,
-	CarParkingPlace NVarChar (20),
-	PositionBonus Double
-)
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Employees')
+BEGIN
+	CREATE TABLE Employees(
+		EmployeeId INT IDENTITY(1, 1) NOT NULL,
+		FirstName NVarChar (20) NOT NULL,
+		LastName NVarChar(20) NOT NULL,
+		PhoneNumber NVarChar(15),
+		Adress XML NOT NULL,
+		PositionId INT FOREIGN KEY (PositionId) REFERENCES dbo.Positions (PositionId),
+		PRIMARY KEY (EmployeeId)
+	)
+END 
+GO
 
-CREATE TABLE Employees(
-	EmployeeId INT IDENTITY(1, 1) NOT NULL,
-	FirstName NVarChar (20) NOT NULL,
-	LastName NVarChar(20) NOT NULL,
-	PhoneNumber NVarChar(15),
-	Adress XML NOT NULL,
-	PositionId INT FOREIGN KEY REFRERENCES Positions(PositionId)
-)
-
-CREATE TABLE Orders(
-	OrderId INT IDENTITY(1, 1) NOT NULL,
-	ClientId INT NOT NULL FOREIGN KEY REFERENCES Clients(ClientId),
-	EmployeeId INT NOT NULL FOREIGN KEY REFERENCES Employees(EmployeeId),
-	OrderDate DateTime,
-	OrderDetails XML NOT NULL,
-	TotalCost Decimal (18,2) NOT NULL
-)
+IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND TABLE_NAME='Orders')
+BEGIN
+	CREATE TABLE Orders(
+		OrderId INT IDENTITY(1, 1) NOT NULL,
+		ClientId INT NOT NULL FOREIGN KEY REFERENCES Clients(ClientId) ON DELETE CASCADE,
+		EmployeeId INT NOT NULL FOREIGN KEY REFERENCES Employees(EmployeeId),
+		OrderDate DateTime,
+		OrderDetails XML NOT NULL,
+		TotalCost Decimal (18,2) NOT NULL,
+		PRIMARY KEY (OrderId)
+	)
+END
+GO
