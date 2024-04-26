1b97cd93da375ca00bf195b7dfc58182e2566280
pawel200020 <pawel200020@gmail.com>
2024-04-26 18:04:32 +0200

    🗃️ entity framework example usage

--- C#/FrameworksPerformance/FrameworksPerformance/Program.cs
+++ C#/FrameworksPerformance/FrameworksPerformance/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using EntityFramework;
+using EntityFramework.Models;
 
 Console.WriteLine("Hello, World!");
-Class1 c1;
+
+
+using var context = new FrameworkPerformanceMtContext();
+
+var test = new SingleTableTests(context);
+test.Run();
